7654bea47b26cf55eac544f523b3fbcec9151cba
Rory <rory88@gmail.com>
2015-11-16 18:40:36 +0000

    init

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('axolotlsApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });

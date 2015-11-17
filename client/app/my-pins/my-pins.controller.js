1c2bd18e051e14efd5200be0d6c6073965e3f9ba
Rory <rory88@gmail.com>
2015-11-17 19:17:42 +0000

    implemented delete functionality on individual pins in `my-pins`

--- client/app/my-pins/my-pins.controller.js
+++ client/app/my-pins/my-pins.controller.js
@@ -34,10 +34,37 @@ angular.module('axolotlsApp')
         });
     };
 
+    $scope.showConfirm = function() {
+      // Appending dialog to document.body to cover sidenav in docs app
+      var confirm = $mdDialog.confirm()
+        .title('Would you like to delete your debt?')
+        .content('All of the banks have agreed to forgive you your debts.')
+        .ok('Please do it!')
+        .cancel('Sounds like a scam');
+      $mdDialog.show(confirm).then(function() {
+        $scope.status = 'You decided to get rid of your debt.';
+      }, function() {
+        $scope.status = 'You decided to keep your debt.';
+      });
+    };
+
+    // User clicked add. Call dialog to add to db.
     $scope.addPin = function () {
       $scope.showAdvanced('AddPinCtrl', 'app/add-pin/add-pin.html', initialGet, null)
     };
 
+    // User clicked delete on a pin. Call dialog to confirm.
+    $scope.deletePin = function (pin) {
+      $scope.showConfirm();
+
+      $http.delete('/api/pins/' + pin._id).success(function () {
+
+      }).error(function (error) {
+        console.log('error:', error);
+      })
+    };
+
+    // User clicked on an image. Call dialog to show big version of the image.
     $scope.bigImage = function (url) {
       $scope.showAdvanced('BigImageCtrl', 'app/big-image/big-image.html', null, url);
     };

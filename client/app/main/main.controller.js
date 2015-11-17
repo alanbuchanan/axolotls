8f442cb560e3bb1dc879e8f5e6d71c9107a8e72d
Rory <rory88@gmail.com>
2015-11-17 17:45:22 +0000

    implemented link to user's page for all pins in `main`

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,11 +4,8 @@ angular.module('axolotlsApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.pins = [];
 
-    for (var i = 0; i < 4; i++) {
-      $scope.pins.push({
-        icon: 'http://c.files.bbci.co.uk/9273/production/_86719473_oneill.jpg',
-        title: "aids"
-      });
-    }
+    $http.get('/api/pins').success(function (pins) {
+      $scope.pins = pins;
+    })
 
   });

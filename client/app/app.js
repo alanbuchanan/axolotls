eb991a07e65c3370138beef6405c31ebd3dc6fb1
Rory <rory88@gmail.com>
2015-11-17 01:20:28 +0000

    implemented angular-material.

--- client/app/app.js
+++ client/app/app.js
@@ -5,7 +5,8 @@ angular.module('axolotlsApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngMaterial'
 ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider

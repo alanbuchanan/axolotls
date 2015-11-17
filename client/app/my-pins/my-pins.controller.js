0d1500849abe36ccf6acb1eebb94646a4276cd77
Rory <rory88@gmail.com>
2015-11-17 13:34:29 +0000

    added dialog for adding a pin

--- client/app/my-pins/my-pins.controller.js
+++ client/app/my-pins/my-pins.controller.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('axolotlsApp')
-  .controller('MyPinsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('MyPinsCtrl', function ($scope, $mdDialog) {
+
+    $scope.showAdvanced = function() {
+      $mdDialog.show({
+        controller: 'AddPinCtrl',
+        templateUrl: 'app/add-pin/add-pin.html',
+        parent: angular.element(document.body),
+        clickOutsideToClose:true
+      })
+        .then(function(answer) {
+          $scope.status = 'You said the information was "' + answer + '".';
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
+        });
+    };
+
+
   });

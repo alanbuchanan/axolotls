f3f1e32a74df8f24d0551ce021d8713cb01cf2d3
Rory <rory88@gmail.com>
2015-11-18 18:47:43 +0000

    fixed title relative to user in view

--- client/app/user-pins/user-pins.controller.js
+++ client/app/user-pins/user-pins.controller.js
@@ -3,15 +3,19 @@
 angular.module('axolotlsApp')
   .controller('UserPinsCtrl', function ($scope, $http, $routeParams, Auth) {
 
-    $scope.getCurrentUser = Auth.getCurrentUser;
-
     $scope.userId = $routeParams._id;
     console.log('id:', $scope.userId);
 
     $scope.pins = [];
+    $scope.username = '';
+
+    $scope.printUsername = function () {
+      return $scope.username + '\'s pins';
+    };
 
     // Get all user's pins
     $http.get('/api/pins/' + $scope.userId).success(function (pins) {
+      $scope.username = pins[0].ownerUsername;
       $scope.pins = pins;
     }).error(function (error) {
       console.log('error:', error);

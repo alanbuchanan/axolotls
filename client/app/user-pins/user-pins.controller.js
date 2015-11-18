5f5cc302a49ab5468d3f4a75bfdaf83b680890e1
Rory <rory88@gmail.com>
2015-11-18 18:31:50 +0000

    improved layout

--- client/app/user-pins/user-pins.controller.js
+++ client/app/user-pins/user-pins.controller.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('axolotlsApp')
-  .controller('UserPinsCtrl', function ($scope, $http, $routeParams) {
+  .controller('UserPinsCtrl', function ($scope, $http, $routeParams, Auth) {
+
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
     $scope.userId = $routeParams._id;
     console.log('id:', $scope.userId);
 
     $scope.pins = [];
 
+    // Get all user's pins
     $http.get('/api/pins/' + $scope.userId).success(function (pins) {
       $scope.pins = pins;
     }).error(function (error) {

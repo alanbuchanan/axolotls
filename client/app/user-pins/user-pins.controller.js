0fe4a53f16addd7c2ae79ddefa7cbdf09a0f8cf9
Rory <rory88@gmail.com>
2015-11-18 18:11:40 +0000

    implemented get only user id's for `user-pins` view

--- client/app/user-pins/user-pins.controller.js
+++ client/app/user-pins/user-pins.controller.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('axolotlsApp')
-  .controller('UserPinsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('UserPinsCtrl', function ($scope, $http, $routeParams) {
+    $scope.userId = $routeParams._id;
+    console.log('id:', $scope.userId);
+
+    $scope.pins = [];
+
+    $http.get('/api/pins/' + $scope.userId).success(function (pins) {
+      $scope.pins = pins;
+    }).error(function (error) {
+      console.log('error:', error);
+    })
+
   });
--- client/app/user-pins/user-pins.js
+++ client/app/user-pins/user-pins.js
@@ -3,7 +3,7 @@
 angular.module('axolotlsApp')
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/user-pins', {
+      .when('/:_id', {
         templateUrl: 'app/user-pins/user-pins.html',
         controller: 'UserPinsCtrl'
       });

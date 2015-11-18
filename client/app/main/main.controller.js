e96604c8eb8ad904c370eacded37aaa696731d44
Rory <rory88@gmail.com>
2015-11-18 19:07:18 +0000

    'submitted by' message improved depending on whether post is user's

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,10 @@
 'use strict';
 
 angular.module('axolotlsApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, Auth) {
+
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
     $scope.pins = [];
 
     $http.get('/api/pins').success(function (pins) {

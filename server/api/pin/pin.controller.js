5c473584480b2e3202e5d603bad759a7f67b88e4
Rory <rory88@gmail.com>
2015-11-18 18:11:51 +0000

    implemented get only user id's for `user-pins` view

--- server/api/pin/pin.controller.js
+++ server/api/pin/pin.controller.js
@@ -4,51 +4,80 @@ var _ = require('lodash');
 var Pin = require('./pin.model');
 
 // Get list of pins
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   Pin.find(function (err, pins) {
-    if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
     return res.status(200).json(pins);
   });
 };
 
 // Get a single pin
-exports.show = function(req, res) {
-  Pin.findById(req.params.id, function (err, pin) {
-    if(err) { return handleError(res, err); }
-    if(!pin) { return res.status(404).send('Not Found'); }
-    return res.json(pin);
-  });
+//exports.show = function(req, res) {
+//  Pin.findById(req.params.id, function (err, pin) {
+//    if(err) { return handleError(res, err); }
+//    if(!pin) { return res.status(404).send('Not Found'); }
+//    return res.json(pin);
+//  });
+//};
+
+// Get a list of pins based on user ID
+exports.show = function (req, res) {
+  Pin.find({ownerId: req.params.id}, function (err, pins) {
+    console.log('Hello from show! Pins:', pins);
+    if (err) {
+      return handleError(res, err);
+    }
+    return res.status(200).json(pins);
+  })
 };
 
 // Creates a new pin in the DB.
-exports.create = function(req, res) {
-  Pin.create(req.body, function(err, pin) {
-    if(err) { return handleError(res, err); }
+exports.create = function (req, res) {
+  Pin.create(req.body, function (err, pin) {
+    if (err) {
+      return handleError(res, err);
+    }
     return res.status(201).json(pin);
   });
 };
 
 // Updates an existing pin in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
+exports.update = function (req, res) {
+  if (req.body._id) {
+    delete req.body._id;
+  }
   Pin.findById(req.params.id, function (err, pin) {
-    if (err) { return handleError(res, err); }
-    if(!pin) { return res.status(404).send('Not Found'); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!pin) {
+      return res.status(404).send('Not Found');
+    }
     var updated = _.merge(pin, req.body);
     updated.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.status(200).json(pin);
     });
   });
 };
 
 // Deletes a pin from the DB.
-exports.destroy = function(req, res) {
+exports.destroy = function (req, res) {
   Pin.findById(req.params.id, function (err, pin) {
-    if(err) { return handleError(res, err); }
-    if(!pin) { return res.status(404).send('Not Found'); }
-    pin.remove(function(err) {
-      if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!pin) {
+      return res.status(404).send('Not Found');
+    }
+    pin.remove(function (err) {
+      if (err) {
+        return handleError(res, err);
+      }
       return res.status(204).send('No Content');
     });
   });

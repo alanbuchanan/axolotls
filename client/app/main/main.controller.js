30f0ed2e1510e6d1da522bb22a500d9fffdb8f7d
Rory <rory88@gmail.com>
2015-11-18 20:36:09 +0000

    initialised masonry grid

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,6 +3,14 @@
 angular.module('axolotlsApp')
   .controller('MainCtrl', function ($scope, $http, Auth) {
 
+
+$('.grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  columnWidth: '.grid-sizer',
+  gutter: 10
+});
     $scope.getCurrentUser = Auth.getCurrentUser;
 
     $scope.pins = [];
